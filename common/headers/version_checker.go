4857238dfe5569c9d6f9a3e287dd417d838ee4ac
Temporal Data <commander-data@temporal.io>
2023-12-01 23:32:57 +0000

    Bump Server version to 1.23.0-rodrigozhou-test-2

--- common/headers/version_checker.go
+++ common/headers/version_checker.go
@@ -47,7 +47,7 @@ const (
 	ClientNameCLI           = "temporal-cli"
 	ClientNameUI            = "temporal-ui"
 
-	ServerVersion = "1.23.0-rodrigozhou-test"
+	ServerVersion = "1.23.0-rodrigozhou-test-2"
 
 	// SupportedServerVersions is used by CLI and inter role communication.
 	SupportedServerVersions = ">=1.0.0 <2.0.0"
